210bf317ba9f71772aff631acd488ea5fc8f8c89
kaypee <kaypee.casensa@gmail.com>
2019-07-24 10:42:56 +0800

    Remove events app in admin dashboard

--- events/admin.py
+++ events/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from events.models import Event, Criteria, Judge, Participant, ParticipantRating
 
-class CriteriaInline(admin.TabularInline):
-	model = Criteria
+# class CriteriaInline(admin.TabularInline):
+# 	model = Criteria
 
-class JudgeInline(admin.TabularInline):
-	model = Judge
+# class JudgeInline(admin.TabularInline):
+# 	model = Judge
 
-class ParticipantInline(admin.TabularInline):
-	model = Participant
+# class ParticipantInline(admin.TabularInline):
+# 	model = Participant
 
-class EventAdmin(admin.ModelAdmin):
-	list_display=('title','is_closed',)
-	inlines = [CriteriaInline, JudgeInline, ParticipantInline]
-admin.site.register(Event, EventAdmin)
-admin.site.register(ParticipantRating)
+# class EventAdmin(admin.ModelAdmin):
+# 	list_display=('title','is_closed',)
+# 	inlines = [CriteriaInline, JudgeInline, ParticipantInline]
+# admin.site.register(Event, EventAdmin)
+# admin.site.register(ParticipantRating)
